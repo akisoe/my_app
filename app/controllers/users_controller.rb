6b1da7c36216cdfff45bb74437a586fda5513de4
akiras <fascinate009@gmail.com>
2015-06-04 03:31:44 +0000

    Finish user index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render 'new'
-    end
+  before_action :authenticate_user!, only: [:index, :edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
